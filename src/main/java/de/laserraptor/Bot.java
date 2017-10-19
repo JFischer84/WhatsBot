67c3ad17b92d681d3bc50d617e5c60c493936b6e
jfische1 <jonas.fischer@immonet.de>
2017-10-19 16:34:12 +0200

    Changed Webdriver to Chrome, still issues with Keys.chord

--- src/main/java/de/laserraptor/Bot.java
+++ src/main/java/de/laserraptor/Bot.java
@@ -1,18 +1,21 @@
 package de.laserraptor;
 
-import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import at.mukprojects.giphy4j.exception.GiphyException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -30,9 +33,9 @@ public class Bot {
 	public void spam(String chatName, String searchTag, int times) throws GiphyException {
 		startUp();
 		driver.get("https://web.whatsapp.com");
-		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.elementToBeClickable(By.className("input-search")));
-		WebElement searchBar = driver.findElement(By.className("input-search"));
+		WebDriverWait wait = new WebDriverWait(driver, 8);
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[title=\"Suchen oder neuen Chat beginnen\"]")));
+		WebElement searchBar = driver.findElement(By.cssSelector("input[title=\"Suchen oder neuen Chat beginnen\"]"));
 		searchBar.click();
 		searchBar.sendKeys(chatName);
 		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[title='" + chatName + "']")));
@@ -45,27 +48,44 @@ public class Bot {
 		for (int i = 0; i < times; i++) {
 			processedUrl = gifProvider.provideRandom(searchTag);
 			selectedElement.sendKeys(processedUrl);
-			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "a"));
-			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "c"));
-			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "v"));
+			Actions actions = new Actions(driver);
 
-			wait.until(ExpectedConditions.elementToBeClickable(By.className("pluggable-input-default")));
+			actions.sendKeys(selectedElement, Keys.chord(Keys.COMMAND, "a"));
 
-			title = driver.findElement(By.className("pluggable-input-default"));
-			title.click();
-			title.sendKeys(Keys.chord(Keys.ENTER));
-			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "a"));
-			selectedElement.sendKeys(Keys.chord(Keys.DELETE));
+
+//			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+//			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "c"));
+//			selectedElement.sendKeys(Keys.chord(Keys.DELETE));
+//			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "v"));
+
+//			wait.until(ExpectedConditions.elementToBeClickable(By.className("pluggable-input-default")));
+//
+//			wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[data-icon='\"send-light\"']")));
+//
+//			driver.findElement(By.cssSelector("span[title='" + chatName + "']")).click();
+//
+//			title = driver.findElement(By.className("pluggable-input-default"));
+//			title.click();
+//			title.sendKeys(Keys.chord(Keys.ENTER));
+//			selectedElement.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+
 		}
 	}
 
 	private void startUp() {
-		System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver_14");
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver_2_33");
 		gifProvider = new GifProvider();
-		ProfilesIni profile = new ProfilesIni();
-		FirefoxProfile firefoxProfile = profile.getProfile(fireFoxUserProfile);
-		FirefoxOptions firefoxOptions = new FirefoxOptions();
-		firefoxOptions.setProfile(firefoxProfile);
-		driver = new FirefoxDriver(firefoxOptions);
+//		ProfilesIni profile = new ProfilesIni();
+//		FirefoxProfile firefoxProfile = profile.getProfile(fireFoxUserProfile);
+//		FirefoxOptions firefoxOptions = new FirefoxOptions();
+//		firefoxOptions.setProfile(firefoxProfile);
+
+		String userProfile = "/Users/jfische1/Library/Application Support/Google/Chrome/Default";
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--user-data-dir=" + userProfile);
+
+
+
+		driver = new ChromeDriver(chromeOptions);
 	}
 }
